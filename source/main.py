8ac97197bd13016d2904a3f7419bd81bcbcafe6e
Paulina Wawrzyniak <pc16439@st.amu.edu.pl>
2022-05-29 09:39:57 +0200

    added style sheet and tabs

--- source/main.py
+++ source/main.py
@@ -78,7 +78,7 @@ VAERSSYMPTOMS = pd.concat(
 
 DATA_VAX = pd.merge(VAERSDATA, VAERSVAX, on="VAERS_ID")
 
-app = Dash(__name__)
+app = Dash(__name__, assets_folder="../assets", suppress_callback_exceptions=True)
 
 df2 = (
     VAERSDATA.round(0).groupby(["AGE_YRS"])["AGE_YRS"].size().reset_index(name="ilosc")
@@ -92,28 +92,108 @@ df3 = (
     .sort_values(by="ilosc")
 )
 vax_names = pd.unique(DATA_VAX["VAX_NAME"].replace(regex={r" \(.*\)$": ""}))
-
 app.layout = html.Div(
+    className="grid-wrapper",
     children=[
-        html.H1(children="Hello Dash"),
-        html.Div(
-            children="""
-        Dash: A web application framework for your data.
-    """
-        ),
-        dcc.Graph(id="graph-with-slider"),
-        dcc.RangeSlider(0, 120, 1, value=[10, 50], id="my-range-slider"),
-        html.Div(id="slider-output-container"),
         html.Div(
             children=[
-                dcc.Graph(id="doctor-graph"),
-                html.Label("Wybierz szczepionke"),
-                dcc.Dropdown(vax_names, "INFLUENZA SEASONAL", id="wybierz-szczepionke"),
+                html.H1(children="Vaccine adverse events", className="header-title"),
+                html.H2(
+                    children="Visualize the adverse event reactions"
+                    " to vaccines in US"
+                    " between 2015 and 2020",
+                    className="header-description",
+                ),
             ],
-            style={"padding": 10, "flex": 1},
+            className="header",
         ),
-    ]
+        html.Div(
+            children=[
+                dcc.Tabs(
+                    id="tabs-with-classes",
+                    value="tab-1",
+                    parent_className="custom-tabs",
+                    className="custom-tabs-container",
+                    children=[
+                        dcc.Tab(
+                            label="Age influence",
+                            value="tab-1",
+                            className="custom-tab",
+                            selected_className="custom-tab--selected",
+                        ),
+                        dcc.Tab(
+                            label="Doctor graph",
+                            value="tab-2",
+                            className="custom-tab",
+                            selected_className="custom-tab--selected",
+                        ),
+                        dcc.Tab(
+                            label="Tab three, multiline",
+                            value="tab-3",
+                            className="custom-tab",
+                            selected_className="custom-tab--selected",
+                        ),
+                        dcc.Tab(
+                            label="Tab four",
+                            value="tab-4",
+                            className="custom-tab",
+                            selected_className="custom-tab--selected",
+                        ),
+                    ],
+                ),
+                html.Div(id="tabs-content-classes"),
+            ],
+            className="graphs",
+        ),
+    ],
 )
+
+
+@app.callback(
+    Output("tabs-content-classes", "children"), Input("tabs-with-classes", "value")
+)
+def render_content(tab):
+    """updating tabs selection"""
+    if tab == "tab-1":
+        return html.Div(
+            [
+                html.Div(
+                    className="graphs-selection",
+                    children=[
+                        html.Label("Select age"),
+                        dcc.RangeSlider(
+                            0, 120, 5, value=[10, 50], id="my-range-slider"
+                        ),
+                    ],
+                ),
+                dcc.Graph(id="graph-with-slider"),
+            ]
+        )
+    if tab == "tab-2":
+        return html.Div(
+            [
+                html.Div(
+                    className="graphs-selection",
+                    children=[
+                        html.Label("Wybierz szczepionke"),
+                        dcc.Dropdown(
+                            vax_names, "INFLUENZA SEASONAL", id="wybierz-szczepionke"
+                        ),
+                    ],
+                ),
+                html.Div(
+                    className="doctors_graph",
+                    children=[
+                        dcc.Graph(id="doctor-graph"),
+                    ],
+                ),
+            ]
+        )
+    if tab == "tab-3":
+        return html.Div([html.H3("Tab content 3")])
+    if tab == "tab-4":
+        return html.Div([html.H3("Tab content 4")])
+    return html.Div([html.H3("NO TAB SELECTED")])
 
 
 @app.callback(
@@ -122,8 +202,9 @@ app.layout = html.Div(
 def update_slider(value):
     """updating line graph based on the slider value"""
     mask = (df2["AGE_YRS"] > value[0]) & (df2["AGE_YRS"] < value[1])
-    fig = px.line(df2[mask], x="AGE_YRS", y="ilosc")
+    fig = px.line(df2[mask], x="AGE_YRS", y="ilosc", template="ggplot2")
     fig.update_layout(transition_duration=500)
+    fig.update_layout(plot_bgcolor="#f6f6f2")
 
     return fig
 
@@ -140,7 +221,9 @@ def update_graph(value):
             "VAX_NAME": "Vaccine for disease: " + value,
             "ilosc": "Number of ER visits",
         },
+        template="ggplot2",
     )
+    fig2.update_layout(plot_bgcolor="#f6f6f2")
     fig2.update_layout()
 
     return fig2
